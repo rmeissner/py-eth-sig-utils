67143f771b726115e925507d00ab579160e4fce6
Richard Meissner <msc.meissner@gmail.com>
2018-09-16 21:54:14 +0200

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,8 @@
 # fail if any commands fails
 set -e
 
-python -m pip install --user --upgrade setuptools wheel
-python -m pip install --user --upgrade twine
+python -m pip install --upgrade setuptools wheel
+python -m pip install --upgrade twine
 
 python setup.py sdist bdist_wheel
 twine upload -u $PYPI_USER -p $PYPI_PASS dist/*
