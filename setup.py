6830b89739c5da26c337637335442127eca45591
Tobias Oberstein <tobias.oberstein@crossbario.com>
2019-10-18 16:26:01 +0200

    fix deps

--- setup.py
+++ setup.py
@@ -1,7 +1,31 @@
+import os
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+reqs = 'requirements.txt'
+dev_reqs = 'requirements-dev.txt'
+
+install_requires = []
+extras_require = {
+    'dev': []
+}
+
+with open(reqs) as f:
+    for line in f.read().splitlines():
+        line = line.strip()
+        if not line.startswith('#'):
+            parts = line.strip().split(';')
+            if len(parts) > 1:
+                print('Warning: requirements line "{}" ignored, as it uses env markers, which are not supported in setuptools'.format(line))
+            else:
+                install_requires.append(parts)
+
+if os.path.exists(dev_reqs):
+    with open(dev_reqs) as f:
+        for line in f.read().splitlines():
+            extras_require['dev'].append(line.strip())
 
 setuptools.setup(
     name="py_eth_sig_utils",
@@ -15,9 +39,8 @@ setuptools.setup(
     url="https://github.com/rmeissner/py-eth-sig-utils",
     packages=setuptools.find_packages(),
     platforms='Any',
-    install_requires=[
-        'eth-abi>=1.1.1',       # MIT license
-    ],
+    install_requires=install_requires,
+    extras_require=extras_require,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
